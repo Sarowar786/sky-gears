b9fa574c09e0cf2dd18c1b8b00a6f0b461e668fd
Saorwar <sarowar2287@gmail.com>
2024-11-15 03:54:32 +0600

    Exclusive section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Header from '@/components/Header';
 import TopHeader from '../components/TopHeader';
 import ImageSlider from '@/components/ImageSlider';
 import bannerImage from '@/assets/images/bannerImage.png'
+import About from '@/components/Exclusive';
+import Exclusive from '@/components/Exclusive';
 
 export default function HomePage() {
   const images = [
@@ -16,7 +18,7 @@ export default function HomePage() {
       <TopHeader />
       <Header />
       <ImageSlider images={images} />
-      
+      <Exclusive/>
     </main>
   );
 }
---
+++ src/components/Exclusive.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import image from "@/assets/images/image.jpg";
+
+export default function Exclusive() {
+  return (
+    <div className="bg-btnColor w-full  mt-5 mb-20">
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        <div>
+          <Image
+            src={image}
+            height={50}
+            width={800}
+            className="w-full max-h-[403px]"
+          ></Image>
+        </div>
+        <div className="lg:pl-9 px-4 py-10 md:py-0 flex flex-col items-start justify-center gap-4 bg-btnColor ">
+          <p className="text-white text-3xl lg:text-4xl font-semibold ">
+            Get Exclusive Custom Parts
+          </p>
+          <p className="text-white w-[458px] text-base font-normal">Sky Gears has been the supplier that aircraft builders, owners, pilots, and aviation businesses have depended on since 1965. We carry a wide selection of aircraft parts.</p>
+          <button className="px-6 py-3 rounded-[4px] text-black bg-white w-[130px] h-[48px]">Apply Now</button>
+        </div>
+      </div>
+    </div>
+  );
+}
